abada3367dde446a060627344adf87003df86e8b
Akash Ramanujam <akashram@umich.edu>
2017-03-18 16:59:53 -0400

    add/subtract

--- rpn.py
+++ rpn.py
@@ -5,6 +5,10 @@ def calculate(arg):
 	for operand in arg.split():
 		if operand == "+":
 			stack.append(stack.pop() + stack.pop())
+		
+		elif operand == "-":
+			stack.append((stack.pop() - stack.pop()) * -1)
+
 		else:
 			stack.append(float (operand))
 	return stack.pop()
