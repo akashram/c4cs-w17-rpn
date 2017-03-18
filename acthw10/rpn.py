40e0d701c532cffd1ecc3683debe5083f69d42b5
Akash Ramanujam <akashram@umich.edu>
2017-03-18 18:34:20 -0400

    final, screw it

--- acthw10/rpn.py
+++ acthw10/rpn.py
@@ -7,7 +7,15 @@ OPERATORS = {
 	'-': operator.sub,
 	'*': operator.mul,
 	'/': operator.truediv,
+	'^': expy,
 }
+
+def expy(arg1, arg2):
+	result = 1;
+	for numMult in range(1, arg2):
+		result = result * arg1;
+	return result
+
 
 
 def calculate(arg):
